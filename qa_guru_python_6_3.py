8c45a235bbd7411f959ca535b66cddf19828203e
Dmitrii Ermakov <ermakov111@mail.ru>
2023-06-12 17:55:43 +0300

    add !!!!!!

--- qa_guru_python_6_3.py
+++ qa_guru_python_6_3.py
@@ -3,5 +3,5 @@ from selene import be, have
 
 def test_open_browser1():
     browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[name="q"]').should(be.blank).type('!!!!yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
